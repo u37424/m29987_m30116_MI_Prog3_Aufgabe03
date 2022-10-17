637a7d53b0bd88fa626062944f6f2d56b1debed7
u37424 <lucaspirka@gmail.com>
2022-10-17 14:57:04 +0200

    Done

--- src/main/java/de/medieninformatik/Threadpools/MyAction.java
+++ src/main/java/de/medieninformatik/Threadpools/MyAction.java
@@ -1,39 +1,59 @@
 package de.medieninformatik.Threadpools;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
 
-public class NormalMerge {
-    private static final int SWITCH_SIZE = 10;
+public class MyAction<T extends Comparable<? super T>> extends RecursiveAction {
 
-    // main function that sorts array[start..end] using merge()
-    static <T extends Comparable<? super T>> void mergeSort(T[] array, int start, int end) {
-        // base case
+    private final int SWITCH_SIZE = 25;
+
+    private volatile T[] array;
+    private final int start;
+    private final int end;
+
+    public MyAction(T[] array) {
+        this(array, 0, array.length - 1);
+    }
+
+    public MyAction(T[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * The main computation performed by this task.
+     */
+    @Override
+    protected void compute() { // base case
         if (start < end) {
             // find the middle point
             int middle = (start + end) / 2;
-            if ((end+1) - start <= SWITCH_SIZE) System.out.println((end+1) - start);
-            else {
-                mergeSort(array, start, middle); // sort first half
-                mergeSort(array, middle + 1, end);  // sort second half
+            if ((end + 1) - start <= SWITCH_SIZE) {
+                NormalInsertionSort.sort(array, start, end);
+            } else {
+                MyAction l = new MyAction<>(array, start, middle); // sort first half
+                MyAction r = new MyAction<>(array, middle +1, end);  // sort second half
+                r.fork();
+                l.compute();
+                r.join();
+                merge(array, start, middle, end);
             }
 
-            // merge the sorted halves
-            merge(array, start, middle, end);
+            // if (!((end+1) - start <= SWITCH_SIZE)) merge(array, start, middle, end);
         }
     }
 
-    // merges two subarrays of array[].
     static <T extends Comparable<? super T>> void merge(T[] array, int start, int middle, int end) {
         ArrayList<T> leftArray = new ArrayList<>(middle - start + 1);
-        ArrayList<T> rightArray = new ArrayList<>(end-middle);
+        ArrayList<T> rightArray = new ArrayList<>(end - middle);
         // fill in left array
         int len = middle - start + 1;
         for (int i = 0; i < len; ++i)
-            leftArray.add(i, array[start+i]);
+            leftArray.add(i, array[start + i]);
 
         // fill in right array
-        len = end-middle;
+        len = end - middle;
         for (int i = 0; i < len; ++i)
             rightArray.add(i, array[middle + 1 + i]);
 
--- src/main/java/de/medieninformatik/Threadpools/ThreadpoolTest.java
+++ src/main/java/de/medieninformatik/Threadpools/ThreadpoolTest.java
@@ -1,16 +1,34 @@
 package de.medieninformatik.Threadpools;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 
 public class ThreadpoolTest {
     public static void main(String[] args) {
-        ComparableTestObject[] array = new ComparableTestObject[20];
+        ComparableTestObject[] array = new ComparableTestObject[200];
         Random r = new Random();
         for (int i = 0; i < array.length; i++) {
-            array[i] = new ComparableTestObject(r.nextInt());
+            array[i] = new ComparableTestObject(r.nextInt(100));
         }
+
+        //NormalMerge.mergeSort(array, 0, array.length-1);
+
+        //Sort
+        final int nThreads = Runtime.getRuntime().availableProcessors();
+        ForkJoinPool fjp = new ForkJoinPool(nThreads);
+        RecursiveAction a = new MyAction(array);
+        fjp.invoke(a);
+
         for (ComparableTestObject i : array) {
             System.out.println(i.getValue());
+        }
+        for (int i = 1; i < array.length; i++) {
+            if (!(array[i].compareTo(array[i - 1]) >= 0)) {
+                System.out.println("Not sorted");
+                break;
+            }
         }
     }
 }
