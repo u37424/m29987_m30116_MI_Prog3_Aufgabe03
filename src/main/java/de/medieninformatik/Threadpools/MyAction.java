452ef1af17cab7b8eac29a8dd4348fd9f1b94425
u37424 <lucaspirka@gmail.com>
2022-10-17 19:23:17 +0200

    Finished Javadoc and more

--- src/main/java/de/medieninformatik/Threadpools/MyAction.java
+++ src/main/java/de/medieninformatik/Threadpools/MyAction.java
@@ -3,11 +3,26 @@ package de.medieninformatik.Threadpools;
 import java.util.Arrays;
 import java.util.concurrent.RecursiveAction;
 
+/**
+ * @author Luca Spirka m29987
+ * @author Sebastian Siebert m30116
+ * @version 1.0 <br>
+ * 2022-10-10 <br>
+ * Programmierung 3
+ * Testat 3
+ * <p>
+ * Diese Klasse sortiert einen Array vergleichbarer Elemente mit einem parallelen Merge Sort, der in den Einzelfeldern <br>
+ * bei einer festgelegten Grenze auf einen linearen InsertionSort wechselt.<br>
+ */
 public class MyAction<T extends Comparable<? super T>> extends RecursiveAction {
 
-    T[] array;
-    final int start, end;
-    static final int SWITCH_SIZE =25;
+    T[] array;  //Array
+    final int start, end;   //Obere und untere Grenze
+    static final int SWITCH_SIZE = 25;  //Groesse ab der ein Feld mit InsertionSort sortiert werden soll.
+
+    MyAction(T[] array) {
+        this(array, 0, array.length);
+    }
 
     MyAction(T[] array, int start, int end) {
         this.array = array;
@@ -15,25 +30,30 @@ public class MyAction<T extends Comparable<? super T>> extends RecursiveAction {
         this.end = end;
     }
 
-    MyAction(T[] array) {
-        this(array, 0, array.length);
-    }
-
+    /**
+     * The main computation performed by this task.
+     */
     protected void compute() {
         if (end - start < 2 || end- start <= SWITCH_SIZE)
-            insertionSort(start, end);
+            insertionSort(start, end);  //Wenn Feld kleiner als SWITCH_SIZE sind, dann InsertionSort
         else {
-            int mid = (start + end) /2;
-            MyAction<T> l = new MyAction<>(array, start, mid);
+            int mid = (start + end) /2; //Teilen
+            MyAction<T> l = new MyAction<>(array, start, mid);  //neue Recursive Actions fuer die Teilbereiche
             MyAction<T> r = new  MyAction<>(array, mid, end);
-            l.fork();
-            r.compute();
-            l.join();
-            merge(start, mid, end);
+            l.fork();   //Abspalten
+            r.compute();    //Berechnen
+            l.join();   //Zusammenfuegen der Actions
+            merge(start, mid, end); //Zusammenfuegen der Teilbereiche im Array
         }
     }
 
+    /**
+     * Sortiert einen Teilarray mit einer Groesse <= SWITCH_SIZE mit InsertionSort.
+     * @param start Startindex des Feldes.
+     * @param end Endindex des Feldes.
+     */
     void insertionSort(int start, int end) {
+        //Insertion Sort aus Prog1
             for (int i = start; i < end; i++) {
                 T einfuegeWert = array[i];
                 int lochPos = i;
@@ -45,7 +65,14 @@ public class MyAction<T extends Comparable<? super T>> extends RecursiveAction {
             }
     }
 
+    /**
+     * Verbindet zwei Teilbereiche eines groesseren Feldes im Array zu einem neuen und sortiertem.
+     * @param lo Untere Grenze des Feldes.
+     * @param mid Mittelpunkt des Feldes, wo das Feld getrennt ist.
+     * @param hi Obere Grenze des Feldes.
+     */
     void merge(int lo, int mid, int hi) {
+        //Zusammenfuegen von beiden Teilfeldern
         T[] temp = Arrays.copyOfRange(array, lo, mid);
         for (int i = 0, j = lo, k = mid; i < temp.length; j++)
             array[j] = (k == hi || temp[i].compareTo(array[k]) < 0) ?
--- src/main/java/de/medieninformatik/Threadpools/ThreadpoolTest.java
+++ src/main/java/de/medieninformatik/Threadpools/ThreadpoolTest.java
@@ -4,29 +4,50 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
-
+/**
+ * @author Luca Spirka m29987
+ * @author Sebastian Siebert m30116
+ * @version 1.0 <br>
+ * 2022-10-10 <br>
+ * Programmierung 3
+ * Testat 3
+ * <p>
+ * Diese Klasse sortiert einen Array vergleichbarer Elemente mithilfe von ForkJoinPool und parallelem Merge Sort / normalen Insertion Sort.<br>
+ * Der sortierte Array wird auf Korrektheit ueberprueft.
+ */
 public class ThreadpoolTest {
+    /**
+     * Ein Array wird mit vergleichbaren Objekten gefuellt und Mithilfe von ForkJoinPool, MergeSort und Insertion Sort sortiert.<br>
+     * Danach wird der Array auf eine korrekte Sortierung ueberprueft.<br>
+     * @param args User argumente.
+     */
     public static void main(String[] args) {
+        //Setup Liste
         ComparableTestObject[] array = new ComparableTestObject[200];
         Random r = new Random();
+
         for (int i = 0; i < array.length; i++) {
             array[i] = new ComparableTestObject(r.nextInt(100));
         }
 
+        //Setup Check
         ComparableTestObject[] check = Arrays.copyOf(array, array.length);
+        Arrays.parallelSort(check);
 
-        //Sort
+        //Setup Sort
         final int nThreads = Runtime.getRuntime().availableProcessors();
         ForkJoinPool fjp = new ForkJoinPool(nThreads);
-        RecursiveAction a = new MyAction(array);
+        RecursiveAction a = new MyAction<>(array);
+
+        //Sort
         fjp.invoke(a);
 
-        Arrays.parallelSort(check);
+        //Check Sort
         boolean cmp = check.length == array.length;
         for (int i = 0; cmp && i < check.length; ++i) {
             cmp = check[i].compareTo(array[i]) == 0;
             System.out.println("A " + array[i].getValue() + " B:" + check[i].getValue());
         }
-        System.out.printf(" Felder stimmen %s überein !", cmp ? "" : "nicht ");
+        System.out.printf("Felder stimmen %süberein!", cmp ? "" : "nicht ");
     }
 }
